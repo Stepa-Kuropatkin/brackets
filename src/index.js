8e45b5fc66269be67d43492aee2ecf4351c7d7cc
stepa-kuropatkin <igortrotsuk@gmail.com>
2021-02-04 16:10:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let openBr = [];
+    let closedBr = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        openBr[i] = bracketsConfig[i][0];
+        closedBr[i] = bracketsConfig[i][1];
+    }
+
+    let arr = str.split("");
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let br in openBr) {
+            if (arr[i] == openBr[br]) {
+                if (arr[i + 1] == closedBr[br]) {
+                    arr.splice(i, 2);
+                    i = -1;
+                    break;
+                }
+            }
+        }
+    }
+    if (arr.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
